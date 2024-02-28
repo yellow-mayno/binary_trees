9387f098146bc93872412e423ca225e80c83e6b6
CB <C@B.m>
2024-02-28 22:31:38 +0100

    create node fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	struct binary_tree_s *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->left = NULL;
+	new_node->right = NULL;
 	new_node->n = value;
 	new_node->parent = parent;
 	return (new_node);
