95f7e03ab53585f078211ed831408d7f1a5efeac
CB <C@B.m>
2024-02-29 14:10:00 +0100

    left insert fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,17 +8,17 @@
  * @value: an integer
  * Return: pointer to the inserted left node
  */
-binary_tree_t binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t new_node;
+	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t);
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
 	new_node->left = parent->left;
-	parent->left = new_node
+	parent->left = new_node;
 	return (new_node);
 }
